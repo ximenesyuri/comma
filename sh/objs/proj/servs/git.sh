dbe7e0451dd3ad73652d18a063dc80106066a1f7
yxm <yxm.dev@gmx.ie>
2025-01-03 14:36:22 -0300

    feat: add basic git operations

--- sh/objs/proj/servs/git.sh
+++ sh/objs/proj/servs/git.sh
@@ -16,14 +16,17 @@ function git_ {
     case "$action" in
         a|add)
             git add .
+            done_ "Changes added to stage in proj '$proj'"
             ;;
         c|commit)
-            if [[ -z "$param" ]]; then
+            shift 2
+            message="$@"
+            if [[ -z "$message" ]]; then
                 error_ "Commit message required."
                 popd > /dev/null
                 return 1
             fi
-            git commit -m "$param"
+            git commit -m "$message"
             ;;
         b|branch)
             if [[ -z "$param" ]]; then
@@ -41,13 +44,24 @@ function git_ {
             fi
             git push origin "$param"
             ;;
-        l|pull)
+        P|pull)
             if [[ -z "$param" ]]; then
                 error_ "Branch name required."
                 popd > /dev/null
                 return 1
             fi
             git pull origin "$param"
+            ;;
+        A|amend)
+            local commit_id=$(git log --oneline | fzf $FZF_GEOMETRY)
+            if [[ -n "$commit_id" ]]; then
+                primary_ "New message:"
+                read -p "> " new_message
+                git commit --amend -m "$new_message" --no-edit
+                done_ "The commit has been amended."
+            else
+                error_ "No commit selected."
+            fi
             ;;
         *)
             error_ "Unsupported git action: $action."
